e94acdc340f759922fe21a9ad8ac3f3156d80845
quocthinh861 <quocthinh861@gmail.com>
2021-11-17 13:07:56 +0700

    .

---
+++ EShop.Data/Data/EShopDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Data.Data
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void ModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+
+    }
+}
